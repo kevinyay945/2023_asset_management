dd8a838204d2bca769fc680ee0be08a6ef1329a7
Kevin Chen <kevinyay945@gmail.com>
2023-09-02 16:35:02 +0800

    refactor: change get config way from func to struct method

--- cmd/start.go
+++ cmd/start.go
@@ -13,15 +13,10 @@ import (
 // startCmd represents the start command
 var startCmd = &cobra.Command{
 	Use:   "start",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "start main program",
+	Long:  `start main program`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("start called", helper.PORT())
+		fmt.Println("start called", helper.Config.Port())
 	},
 }
 
--- helper/config.go
+++ helper/config.go
@@ -2,6 +2,23 @@ package helper
 
 import "github.com/spf13/viper"
 
-var PORT = func() string {
+var Config Configer
+
+type Configer interface {
+	Port() string
+}
+
+type config struct {
+}
+
+func (m *config) Port() string {
 	return viper.GetString("PORT")
+}
+
+func newConfig() Configer {
+	return &config{}
+}
+
+func init() {
+	Config = newConfig()
 }
